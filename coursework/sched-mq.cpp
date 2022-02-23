9a848091f6200c35c44ea5d290948130e59ded01
Dale Huang <huangzihan98@gmail.com>
2022-02-23 17:34:14 +0000

    implemented remove_from_runqueue
    initial implementation of remove_from_runqueue

--- coursework/sched-mq.cpp
+++ coursework/sched-mq.cpp
@@ -77,6 +77,52 @@ public:
     void remove_from_runqueue(SchedulingEntity& entity) override
     {
         // TODO: Implement me!
+        
+        // disable interrupts before modifying runqueue:
+        UniqueIRQLock l;
+
+        // based on the entity's priority, remove from the appropriate runqueue:
+        switch(entity.priority()) {
+            case SchedulingEntityPriority::REALTIME:
+                if (rqRealtime.empty()) {
+                    //do nothing
+                    syslog.messagef(LogLevel::ERROR, "Realtime runqueue is empty! Scheduling entity not removed.");
+                } else {
+                    rqRealtime.remove(&entity);
+                }
+                break;
+
+            case SchedulingEntityPriority::INTERACTIVE:
+                if (rqInteractive.empty()) {
+                    //do nothing
+                    syslog.messagef(LogLevel::ERROR, "Interactive runqueue is empty! Scheduling entity not removed.");
+                } else {
+                    rqInteractive.remove(&entity);
+                }
+                break;
+
+            case SchedulingEntityPriority::NORMAL:
+                if (rqNormal.empty()) {
+                    //do nothing
+                    syslog.messagef(LogLevel::ERROR, "Normal runqueue is empty! Scheduling entity not removed.");
+                } else {
+                    rqNormal.remove(&entity);
+                }
+                break;
+
+            case SchedulingEntityPriority::DAEMON:
+                if (rqDaemon.empty()) {
+                    //do nothing
+                    syslog.messagef(LogLevel::ERROR, "Daemon runqueue is empty! Scheduling entity not removed.");
+                } else {
+                    rqDaemon.remove(&entity);
+                }
+                break;
+            default:
+                // do nothing
+                break;
+        } 
+
     }
 
     /**
